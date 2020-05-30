389bd21f6404c268430bedcb4a5e5226fe04e5ce
Андрей <cereperec@gmail.com>
2020-05-30 22:30:42 +0300

    v0.9

--- gulpfile.js
+++ gulpfile.js
@@ -61,7 +61,7 @@ gulp.task("sprite", function () {
 
 gulp.task("copy", function() {
   return gulp.src([
-    "source/fonts/**/*.{woff, woff2}",
+    "source/fonts/**/*.{woff,woff2}",
     "source/img/**",
     "source/js/**",
   ], {
